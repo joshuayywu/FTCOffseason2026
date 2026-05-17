26098c6c70094d78e8101e9aafcceff3e5924f77
beneben <bchen5844@gmail.com>
2026-05-17 15:48:32 -0400

    turret stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/subsystems/Turret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/subsystems/Turret.java
@@ -1,4 +1,18 @@
 package org.firstinspires.ftc.teamcode.config.subsystems;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class Turret {
+    private DcMotorEx turret;
+
+    public Turret(HardwareMap hardwareMap) {
+        turret = hardwareMap.get(DcMotorEx.class, "turret");
+
+        turret.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        turret.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        turret.setDirection(DcMotor.Direction.FORWARD);
+
+    }
 }
